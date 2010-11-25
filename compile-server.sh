9b0becf006dc3b5d93881245ce53d1f3efd37fed
Sampo Savolainen <sampo.savolainen@spatineo.com>
2010-11-25 11:05:13 +0000

    automatic getdeps if missing
    git-svn-id: https://bsmr.googlecode.com/svn/trunk@60 a8e7774a-6247-822a-e7a0-8d62fd04c088

--- compile-server.sh
+++ compile-server.sh
@@ -5,6 +5,15 @@ for FILE in $(find master/jetty-*/lib/*); do
 	CP=$CP":"$FILE
 done
 
+if [ "$CP" = "" ]; then
+    cd master/
+    ./getdeps.sh
+    echo '#############################################################'
+    echo ' re-run ./compile-server.sh'
+    echo '#############################################################'
+    exit
+fi
+
 DEST=master/bin/
 
 javac -classpath $CP -d $DEST -sourcepath master/src/ $(find master/src/ -name "*.java")

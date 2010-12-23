20f809ff29aa3e97ebb0376834080bfd55590c61
Sampo Savolainen <sampo.savolainen@spatineo.com>
2010-12-23 12:18:37 +0000

    a simple command line word counter
    git-svn-id: https://bsmr.googlecode.com/svn/trunk@152 a8e7774a-6247-822a-e7a0-8d62fd04c088

---
+++ master/src/SimpleWordCount.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.GZIPInputStream;
+
+
+public class SimpleWordCount
+{
+	private Map<String, MutableInteger> counts;
+	private BufferedReader br;
+	private String regex;
+	
+	public SimpleWordCount(InputStream is, String regex)
+	{
+		this.regex = regex;
+		this.counts = new HashMap<String, MutableInteger>();
+		this.br = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+	}
+	
+	public void count() throws IOException
+	{
+		String s;
+		
+		while ( (s = br.readLine()) != null) {
+			String [] tmp = s.split(regex);
+		
+			for (String word : tmp) {
+				MutableInteger mi = counts.get(word);
+				if (mi == null) {
+					mi = new MutableInteger();
+					counts.put(word, mi);
+				}
+				mi.n++;
+			}
+			
+		}
+	}
+	
+	public class MutableInteger
+	{
+		public int n;
+		
+		public MutableInteger()
+		{
+			n = 0;
+		}
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException
+	{
+		String file = "data/data.txt.gz";
+		FileInputStream fis = new FileInputStream(file);
+		GZIPInputStream gzis = new GZIPInputStream(fis);
+		SimpleWordCount swc = new SimpleWordCount(gzis, "\\W+");
+		
+		System.out.println("Calculating..");
+		swc.count();
+		
+		System.out.println("Done! Write the name of the key to check word count");
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s;
+		System.out.print("> "); System.out.flush();
+		while ( (s = br.readLine()) != null) {
+			
+			
+			String key = s; // .trim();
+			
+			MutableInteger mi = swc.counts.get(key);
+			System.out.println("'"+key+"': "+(mi != null ? mi.n : "N/A"));
+			
+			System.out.print("> "); System.out.flush();
+		}
+
+	}
+
+}

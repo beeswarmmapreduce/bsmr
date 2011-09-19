19dd58430075897a97335f53c5da3154a65c0528
Toni Ruottu <toni.ruottu@iki.fi>
2011-09-19 20:44:39 +0000

    wordinput that may work
    git-svn-id: https://bsmr.googlecode.com/svn/trunk@225 a8e7774a-6247-822a-e7a0-8d62fd04c088

---
+++ worker/lib/wordinput.js
@@ -0,0 +1,80 @@
+function wordInput(M) {
+
+    var URL = 'http://localhost:8080/fs/filesystem';
+    var FILENAME = "teksti.txt";
+
+    var maxlen = 10; 
+
+    function Input(M) {
+        this.M = M;
+    }
+
+    Input.prototype.computeSizes = function(splitId, target) {
+        if (typeof(this.fullSize) == typeof(undefined)) {
+            var input = this;
+            var request = new XMLHttpRequest();
+            request.open('GET', URL + '?operation=sizeof&filename=' + FILENAME, true);  
+            request.onreadystatechange = function() {  
+                if (request.readyState == 4) {  
+                    if(request.status == 200) {
+                        var fullSize = JSON.parse(request.responseText);
+                        input.fullSize = fullSize;
+                        input.blockSize = Math.ceil(fullSize / input.M);
+                        input.feed2(splitId, target)
+                    }
+                }  
+            };  
+            request.send(null);
+        }
+        else {
+            this.feed2(splitId, target)
+        }
+    }
+
+    Input.prototype.feed = function(splitId, target) {
+        this.computeSizes(splitId, target)
+    }
+
+    Input.prototype.feed2 = function(splitId, target) {
+
+        var lengthOK = function(x) {
+            var l = x.length;
+            return  0 < l && l <= maxlen;
+        }
+        var makepair = function(x) {
+            return [FILENAME, x];
+        }
+        var feedWords = function(splitId, text, extra) {
+            var words = text.split(/\s+/);
+            var last = words.pop();
+            var next = extra.split(/\s+/).shift();
+            words.push(last + next);
+            words.shift(); //discard first item
+            var shrt = words.filter(lengthOK);
+            var pairs = shrt.map(makepair);
+            target.write(splitId, pairs);
+        }
+        var blockSize = this.blockSize;
+        var begin = splitId * this.blockSize;
+        var wanted = blockSize + maxlen;
+        var left = this.fullSize - begin;
+        var length = Math.min(wanted, left);
+
+        var request = new XMLHttpRequest();  
+        request.open('GET', URL + '?operation=read&filename=' + FILENAME + '&begin=' + begin + '&length=' + length, true);  
+        request.onreadystatechange = function() {  
+            if (request.readyState == 4) {  
+                if(request.status == 200) {
+                    var raw = request.responseText;
+                    var prefix = splitId == 0 ? " " : "";
+                    var text = prefix + raw.slice(0, blockSize);
+                    var extra = raw.slice(blockSize, wanted);
+                    feedWords(splitId, text, extra);
+                }
+            }  
+        };  
+        request.send(null);  
+    }
+    return new Input(M);
+}
+   

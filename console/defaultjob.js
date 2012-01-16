1aec716a7c1ad69c4ac29a870a9e6cbb452ceabe
Toni Ruottu <toni.ruottu@iki.fi>
2012-01-16 20:32:30 +0000

    new console
    git-svn-id: https://bsmr.googlecode.com/svn/trunk@243 a8e7774a-6247-822a-e7a0-8d62fd04c088

---
+++ console/defaultjob.js
@@ -0,0 +1,29 @@
+mapper = function(pair, emit) {
+    var word = pair[1];
+    emit(word, '1');
+}
+
+reducer = function() {
+    function Adder() {
+        this.x = 0;
+    }
+    Adder.prototype = new Generator();
+    Adder.prototype._step = function(y) {
+        if (y) {
+            this.x += parseInt(y, 10);
+        }
+        else {
+            this._lastrun();
+            var result = this.x.toString();
+            this._emit(result);
+        }
+    }
+    return new Adder();
+}
+
+combiner = reducer;
+input = debugInput;
+inter = noInter;
+output = consoleOut;
+chooseBucket = boringBucket;
+
